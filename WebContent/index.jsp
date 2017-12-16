e2049927f10141772e04d15c37687f0007394bfe
KyungjunMin <manonflame2@gmail.com>
2017-12-16 22:31:29 +0900

    till ch4

---
+++ WebContent/index.jsp
@@ -0,0 +1,34 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name = "viewport" content = "width-device-width, initial-scale-1">
+<link rel = "stylesheet" href = "css/bootstrap.css">
+<title>JSP AJAX</title>
+<script src = "http://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src = "js/bootstrap.js"></script>  
+</head>
+<body>
+	<br>
+	<div class = "container">
+		<div class="form-group row pull-right">
+			<div class="col-xs-8">
+				<input class = "form-control" type="text" size="20">
+			</div>
+			<div class="col-xs-2">
+				<button class="btn btn-primary" type="button">검</button>
+			</div>
+		</div>
+		<table class = "table" style="text-align: center; border : ipx = solid id = #dddddd">
+			<thead>
+				<tr>
+					<th style = "background-color: #fafafa; text-align: center;">이름</th>
+					<th style = "background-color: #fafafa; text-align: center;">나이</th>
+					<th style = "background-color: #fafafa; text-align: center;">성별</th>
+					<th style = "background-color: #fafafa; text-align: center;">이메일</th>
+		</table>
+	</div>
+
+</body>
+</html> 
---
+++ src/user/UserSearchServlet.java
@@ -0,0 +1,41 @@
+package user;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserSearchServlet")
+public class UserSearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); //udf-8으로 매개변수를 인코딩함 : 매개변수란 userSearchServle으로 넘어오는 매변수를 말한다.
+		response.setContentType("text/html;charset=UTF-8");
+		String userName = request.getParameter("userName");
+		response.getWriter().write(getJSON(userName));
+	}
+	
+	//검색한 정보가 json형태로 출력되도록 함.
+	public String getJSON(String userName) {
+		if(userName == null) userName ="";
+		StringBuffer result = new StringBuffer("");
+		result.append("{\result\":[");
+		UserDAO userDAO = new UserDAO();
+		ArrayList<User> userList = userDAO.search(userName);
+		for(int i= 0; i < userList.size(); i++) {
+			result.append("[{\"value\": \"" + userList.get(i).getUserName()+"\"},");
+			result.append("{\"value\": \"" + userList.get(i).getUserName()+"\"},");
+			result.append("{\"value\": \"" + userList.get(i).getUserName()+"\"},");
+			result.append("{\"value\": \"" + userList.get(i).getUserName()+"\"}],");
+		}
+		result.append("]}");
+		return result.toString();
+	}
+
+}
